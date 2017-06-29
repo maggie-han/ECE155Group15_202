962faa10100070e8e2bff5f4358de1d5f558f191
maggie-han <maggie.hjx@gmail.com>
2017-06-29 09:38:25 -0400

    added abstract block template

---
+++ app/src/main/java/uwaterloo/ca/ece155group15_202/GameBlockTemplate.java
@@ -0,0 +1,10 @@
+package uwaterloo.ca.ece155group15_202;
+
+/**
+ * Created by Maggie on 2017-06-29.
+ */
+
+public abstract class GameBlockTemplate {
+    public abstract void setDestination();
+    public abstract void move();
+}
